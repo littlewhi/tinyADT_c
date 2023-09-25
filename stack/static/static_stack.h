9b41464519e3fc062dbf512403dc70a22142ba0e
yhb <3157470765@qq.com>
2023-09-25 20:08:08 +0800

    add destroy function

--- stack/static/static_stack.h
+++ stack/static/static_stack.h
@@ -3,43 +3,43 @@
 
 #define GENERIC_STATIC_STACK_STRUCT( element_type, suffix, stack_size ) \
     struct stack_##suffix{                                                \
-        int     size;                                                   \
-        int     index;\
-        element_type data[stack_size];\
+        int     _size;                                                   \
+        int     _index;\
+        element_type _data[stack_size];\
         int (*is_empty)( struct stack_##suffix *self );\
         int (*is_full)( struct stack_##suffix *self );\
         void (*push)( struct stack_##suffix *self, element_type e );\
         void (*pop)( struct stack_##suffix *self );   \
-        element_type (*top)( struct stack_##suffix *self);\  
+        element_type (*top)( struct stack_##suffix *self);\
     }
                                                         
 #define GENERIC_STATIC_STACK_METHOD( element_type, suffix, stack_size )\
 int is_empty_##suffix( struct stack_##suffix *self )\
 {\
-    return self->index == -1;\
+    return self->_index == -1;\
 }\
 \
 int is_full_##suffix( struct stack_##suffix *self )\
 {\
-    return self->index + 1 == self->size;\
+    return self->_index + 1 == self->_size;\
 }\
 \
 void push_##suffix( struct stack_##suffix *self, element_type e )\
 {\
     assert( !self->is_full( self ) );\
-    self->data[++(self->index)] = e;\
+    self->_data[++(self->_index)] = e;\
 }\
 \
 void pop_##suffix( struct stack_##suffix *self )\
 {\
     assert( !self->is_empty( self ) );\
-    self->index -= 1;\
+    self->_index -= 1;\
 }\
 \
 element_type top_##suffix( struct stack_##suffix *self )\
 {\
     assert( !self->is_empty( self ) );\
-    return self->data[(self->index)--];\
+    return self->_data[(self->_index)--];\
 }\
 \
 struct stack_##suffix *get_object_##suffix( void ) \
@@ -47,14 +47,20 @@ struct stack_##suffix *get_object_##suffix( void ) \
     struct stack_##suffix *ret = malloc( sizeof( struct stack_##suffix ) );\
     if( ret == NULL )\
         return NULL;\
-    ret->size = stack_size;\
-    ret->index = -1;\
+    ret->_size = stack_size;\
+    ret->_index = -1;\
     ret->is_empty = is_empty_##suffix;\
     ret->is_full = is_full_##suffix;\
     ret->push = push_##suffix;\
     ret->pop = pop_##suffix;\
     ret->top = top_##suffix;\
     return ret;\
+}\
+\
+void destroy_stack_##suffix( struct stack_##suffix *p )\
+{\
+    free( p );\
 }
 
-#define GENERIC_STATIC_STACK_OBJECT get_object_##suffix()
+#define GENERIC_STATIC_STACK_OBJECT( suffix ) get_object_##suffix()
+#define DESTROY_STATIC_STACK_OBJECT( suffix, p ) destroy_stack_##suffix( p )
