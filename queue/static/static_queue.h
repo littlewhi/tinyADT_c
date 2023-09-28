5b8c1c989103a13da1ccbe8d676574ce5c19e4c8
yhb <3157470765@qq.com>
2023-09-28 10:09:34 +0800

    correct static queue data structure

--- queue/static/static_queue.h
+++ queue/static/static_queue.h
@@ -5,15 +5,13 @@
 struct queue_##suffix{\
     int _front, _tail;\
     int _size;\
-    element_type _data[queue_size];\
+    element_type _data[queue_size + 1];\
     int (*is_empty)( struct queue_##suffix *self );\
     int (*is_full)( struct queue_##suffix *self );\
     element_type (*get_front)( struct queue_##suffix *self );\
     element_type (*get_tail)( struct queue_##suffix *self );\
-    void (*push_front)( struct queue_##suffix *self, element_type e);\
-    void (*push_tail)( struct queue_##suffix *self, element_type e);\
-    void (*pop_front)( struct queue_##suffix *self );\
-    void (*pop_tail)( struct queue_##suffix *self );\
+    void (*push)( struct queue_##suffix *self, element_type e);\
+    void (*pop)( struct queue_##suffix *self );\
 }
 
 #define GENERIC_STATIC_QUEUE_METHOD( element_type, suffix, queue_size )\
@@ -39,45 +37,31 @@ element_type get_tail_##suffix( struct queue_##suffix *self )\
     return self->_data[( self->_tail - 1 + self->_size ) % self->_size];\
 }\
 \
-void push_front_##suffix( struct queue_##suffix *self, element_type e )\
-{\
-    assert( !self->is_full( self ) );\
-    self->_front = ( self->_front - 1 + self->_size ) % self->_size;\
-    self->_data[self->_front] = e;\
-}\
-\
-void push_tail_##suffix( struct queue_##suffix *self, element_type e)\
+void push_##suffix( struct queue_##suffix *self, element_type e)\
 {\
     assert( !self->is_full( self ) );\
     self->_data[self->_tail] = e;\
     self->_tail += 1;\
 }\
 \
-void pop_front_##suffix( struct queue_##suffix *self )\
+void pop_##suffix( struct queue_##suffix *self )\
 {\
     assert( !self->is_empty( self ) );\
     self->_front = ( self->_front + 1 ) % self->_size;\
 }\
 \
-void pop_tail_##suffix( struct queue_##suffix *self )\
-{\
-    assert( !self->is_empty( self ) );\
-    self->_tail = ( self->_tail - 1 + self->_size ) % self->_size;\
-}\
-\
 struct queue_##suffix *get_static_queue_##suffix( void )\
 {\
     struct queue_##suffix *ret = (struct queue_##suffix *)malloc( sizeof( struct queue_##suffix ) );\
-    ret->_size = queue_size;\
+    ret->_size = queue_size + 1;\
     ret->_front = 0;\
     ret->_tail = 0;\
     ret->is_empty = is_empty_##suffix;\
     ret->is_full = is_full_##suffix;\
     ret->get_front = get_front_##suffix;\
     ret->get_tail = get_tail_##suffix;\
-    ret->push_front = push_front_##suffix;\
-    ret->pop_front = pop_front_##suffix;\
-    ret->pop_tail = pop_tail_##suffix;\
+    ret->push = push_##suffix;\
+    ret->pop = pop_##suffix;\
     return ret;\
 }\
 \
